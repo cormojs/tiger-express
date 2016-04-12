791cc73b398df8416cbfd78bd22dff717ba62ced
cormojs <cormorant2+github@gmail.com>
2016-04-13 02:20:25 +0900

    エラーの出力を修正

--- src/type.ml
+++ src/type.ml
@@ -4,23 +4,30 @@ module rec Type : sig
   | Nil
   | Int
   | String
-  | Array of t * unique 
+  | Array of t * unique
   | Name of string * t option ref
   | Unit
   and unique = string
+  val peel : t -> t
   val show : t -> string
   val pp : Format.formatter -> t -> unit
-  val peel : t -> t
-end = struct 
+end = struct
   type t =
   | Record of t M.t * unique
   | Nil
   | Int
   | String
-  | Array of t * unique 
+  | Array of t * unique
   | Name of string * t option ref
   | Unit
   and unique = string
+  let peel = function
+    | Name (id, t_op_ref) as named -> (match !t_op_ref with
+      | Some ty -> ty
+      | None ->
+	Format.eprintf "cannot peel@.";
+	named)
+    | ty -> ty
   let rec show = function
     | Record (m, _) ->
       let lst = M.bindings m in
@@ -35,14 +42,9 @@ end = struct
     | Name (id, t_op_ref) -> "Named " ^ id
     | Unit -> "Unit"
   and pp fmt t = Format.fprintf fmt "Type.%s" @@ show t
-  let peel = function
-    | Name (id, t_op_ref) as named -> (match !t_op_ref with
-      | Some ty -> ty
-      | None -> named)
-    | ty -> ty
 end
 and M : Map.S with type key = string
-  = Map.Make(struct 
+  = Map.Make(struct
     type t = string
     let compare = String.compare
   end)
