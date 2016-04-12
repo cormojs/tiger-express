3fd7969ca70f4c030e81c4901634350e5ebf362a
cormojs <cormorant2+github@gmail.com>
2016-04-13 02:11:13 +0900

    α変換で型宣言を考慮するようにした

--- src/alpha.ml
+++ src/alpha.ml
@@ -1,62 +1,64 @@
 open KNormal
 
-let find x env = 
-  try M.find x env 
+let find x venv = 
+  try M.find x venv 
   with Not_found -> x
 
-let alpha_convert_lval env = function 
+let alpha_convert_lval venv = function 
   | SimpleVar name ->
-    SimpleVar (find name env)
+    SimpleVar (find name venv)
   | SubscriptVar (name, sub) ->
-    SubscriptVar (find name env, find sub env)
+    SubscriptVar (find name venv, find sub venv)
   | FieldVar (name, fname) -> 
-    FieldVar (find name env, fname)
+    FieldVar (find name venv, fname)
 
-let rec alpha_convert_dec env = function
+let rec alpha_convert_dec tenv venv = function
   | VarDec (name, typ, exp) ->
     let name' = Id.genid name in
-    VarDec (name', typ, alpha_convert env exp), M.add name name' env
-  | TypeDecs _ as ty_decs -> ty_decs, env
+    VarDec (name', typ, alpha_convert tenv venv exp), 
+    tenv, M.add name name' venv
+  | TypeDecs tydecs ->
+    TypeDecs tydecs, tenv, venv
   | FunctionDecs fundecs ->
-    let entry env (name, _, _, _) = 
-      M.add name (Id.genid name) env in
-    let env' = List.fold_left entry env fundecs in
-    let convert env (name, args, ret_ty, exp) =
-      let args' = List.map (fun (x, y) -> find x env, y) args in
-      find name env, args', ret_ty, alpha_convert env exp in
-    let fundecs' = List.map (convert env') fundecs in
-    FunctionDecs fundecs', env'
+    let entry venv (name, _, _, _) = 
+      M.add name (Id.genid name) venv in
+    let venv' = List.fold_left entry venv fundecs in
+    let convert venv (name, args, ret_ty, exp) =
+      let args' = List.map (fun (x, y) -> find x venv, y) args in
+      find name venv, args', ret_ty, alpha_convert tenv venv exp in
+    let fundecs' = List.map (convert venv') fundecs in
+    FunctionDecs fundecs', tenv, venv'
 
-and alpha_convert env = function
+and alpha_convert tenv venv = function
   | Unit -> Unit
   | Int i -> Int i
   | String s -> String s
-  | Add (x, y) -> Add (find x env, find y env)
-  | Sub (x, y) -> Sub (find x env, find y env)
-  | Mul (x, y) -> Mul (find x env, find y env)
-  | Div (x, y) -> Div (find x env, find y env)
-  | Call (name, args) -> 
-    Call (find name env, List.map (fun a -> find a env) args)
-  | Var lval -> Var (alpha_convert_lval env lval)
+  | Add (x, y) -> Add (find x venv, find y venv)
+  | Sub (x, y) -> Sub (find x venv, find y venv)
+  | Mul (x, y) -> Mul (find x venv, find y venv)
+  | Div (x, y) -> Div (find x venv, find y venv)
+  | Call (name, args) ->
+    Call (find name venv, List.map (fun a -> find a venv) args)
+  | Var lval -> Var (alpha_convert_lval venv lval)
   | If ((op, x, y), then_exp, else_exp) ->
-    If ((op, find x env, find y env),
-	alpha_convert env then_exp,
-	alpha_convert env else_exp)
+    If ((op, find x venv, find y venv),
+	alpha_convert tenv venv then_exp,
+	alpha_convert tenv venv else_exp)
   | CallExt (name, args) ->
-    CallExt (name, List.map (fun a -> find a env) args)
-  | Assign (lval, name) -> 
-    Assign (alpha_convert_lval env lval, find name env)
-  | Sequence exps -> 
-    Sequence (List.map (alpha_convert env) exps)
+    CallExt (name, List.map (fun a -> find a venv) args)
+  | Assign (lval, name) ->
+    Assign (alpha_convert_lval venv lval, find name venv)
+  | Sequence exps ->
+    Sequence (List.map (alpha_convert tenv venv) exps)
   | While ((op, x, y), body_exp) ->
-    While ((op, find x env, find y env),
-	   alpha_convert env body_exp)
+    While ((op, find x venv, find y venv),
+	   alpha_convert tenv venv body_exp)
   | Break -> Break
   | Record (fields, rec_name) ->
-    let convert (fname, name) = (fname, find name env) in
+    let convert (fname, name) = (fname, find name venv) in
     Record (List.map convert fields, rec_name)
-  | Let (dec, exp) -> 
-    let (dec', env') = alpha_convert_dec env dec in
-    Let (dec', alpha_convert env' exp)
+  | Let (dec, exp) ->
+    let (dec', tenv', venv') = alpha_convert_dec tenv venv dec in
+    Let (dec', alpha_convert tenv' venv' exp)
 
-let f = alpha_convert M.empty
+let f = alpha_convert M.empty M.empty
